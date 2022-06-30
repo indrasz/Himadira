1d4f1845dc2b62f66fbfcdf8e0cfdcc039743ac3
wahyuindra <wahyuindra712@gmail.com>
2022-06-30 13:40:19 +0700

    add title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,16 @@
 <head>
   @include('includes.admin.meta')
 
+
   <title>
-    Argon Dashboard 2 by Creative Tim
+    @yield('title') | Himadira
   </title>
 
   @include('includes.admin.style')
 
 </head>
 
-<body class="g-sidenav-show   bg-gray-100">
+<body class="g-sidenav-show bg-gray-100">
   <div class="min-height-300 bg-primary position-absolute w-100"></div>
 
   @include('includes.admin.sidebar')
