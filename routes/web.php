113b468b7da061083b39c4de85875eced2a7fa31
wahyuindra <wahyuindra712@gmail.com>
2022-07-08 15:28:44 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\InfoController;
+use App\Http\Controllers\Admin\StoreController;
+use App\Http\Controllers\Admin\AdviceController;
+use App\Http\Controllers\Admin\CreationController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\DepartmentController;
+use App\Http\Controllers\Admin\DocumentationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +28,13 @@ Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
     Route::name('dashboard.')->prefix('dashboard')->group(function () {
         Route::middleware(['auth', 'admin'])->group(function () {
             Route::get('/', [DashboardController::class, 'index'])->name('index');
+            Route::resource('store', StoreController::class);
+            Route::resource('advice', AdviceController::class);
+            Route::resource('department', DepartmentController::class);
+            Route::resource('creation', CreationController::class);
+            Route::resource('info', InfoController::class);
+            Route::resource('documentation', DocumentationController::class);
+
         });
     });
 });
