e8222fde7819244b5b15c7c61cf87b99bed87cad
wahyuindra <wahyuindra712@gmail.com>
2022-07-25 13:20:59 +0700

    change logo

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -2,8 +2,8 @@
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="# " target="_blank">
-        <img src="{{ asset('admin/assets/img/logo-ct-dark.png') }}" class="navbar-brand-img h-100" alt="main_logo">
-        <span class="ms-1 font-weight-bold">Himadira</span>
+        <img src="{{ asset('frontend/images/main-logo.png') }}" class="navbar-brand-img h-100" alt="main_logo">
+        <span class="ms-3 font-weight-bold">Himadira</span>
       </a>
     </div>
     <hr class="horizontal dark mt-0">
